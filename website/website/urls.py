a3f8b831bde079cf533e2f1f0b7418d5698a78d3
DanielJMPinto <dmatiaspinto@ua.pt>
2020-04-02 17:38:01 +0100

    template connected to django

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,4 +1,4 @@
-"""CheckMeWOD URL Configuration
+"""website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -19,3 +19,21 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.urls import include
+
+urlpatterns = [
+    path('checkmewod/', include('checkmewod.urls')),
+]
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='checkmewod/', permanent=True)),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
