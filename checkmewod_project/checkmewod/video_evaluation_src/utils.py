3f95ef48178a8d874ea1065ccee0631b4a4e1fce
franciscojesus <franciscojesus@ua.pt>
2020-06-07 22:12:31 +0100

    changes to utils.py

--- checkmewod_project/checkmewod/video_evaluation_src/utils.py
+++ checkmewod_project/checkmewod/video_evaluation_src/utils.py
@@ -1,12 +1,12 @@
 def check_close(pos1, pos2):
-    if pos2 - 20 <= pos1 <= pos2 + 20:
+    if pos2 - 50 <= pos1 <= pos2 + 50:
         return True
     else:
         return False
 
 
 def check_close2(pos1, pos2):
-    if pos2 - 200 <= pos1 <= pos2 + 200:
+    if pos2 - 30 <= pos1 <= pos2 + 30:
         return True
     else:
         return False
@@ -14,6 +14,13 @@ def check_close2(pos1, pos2):
 
 def check_close3(pos1, pos2):
     if pos2 - 30 <= pos1 <= pos2 + 30:
+        return True
+    else:
+        return False
+
+
+def check_close4(pos1, pos2):
+    if pos2 - 35 <= pos1 <= pos2 + 35:
         return True
     else:
         return False
